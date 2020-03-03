81bf9c6d857b6d3ad794f00e7b4da5c278a5a33a
Ariane Holford <aribomop@gmail.com>
2020-03-03 14:41:18 +0000

    add delete method

--- console.rb
+++ console.rb
@@ -1,9 +1,15 @@
 require('pry-byebug')
 require_relative('models/property.rb')
 
+Property.delete_all
 
 property1 = Property.new({'address' => '1 Morrison Street', 'value' => 200000, 'year_built' => 1901, 'for_sale' => true})
 property2 = Property.new({'address' => '3 Miller Place', 'value' => 350000, 'year_built' => 1889, 'for_sale' => true})
 property3 = Property.new({'address' => '49 Buccleuch Street', 'value' => 160000, 'year_built' => 1980, 'for_sale' => false})
 
 property1.save
+property2.save
+property3.save
+
+property3.address = "56 Buccleuch Street"
+property3.update
--- models/property.rb
+++ models/property.rb
@@ -23,4 +23,35 @@ attr_reader :id
     db.close
   end
 
+  def delete
+    db = PG.connect( {dbname: 'property_agency', host: 'localhost'} )
+    sql = "DELETE FROM properties WHERE id = $1"
+    values = [@id]
+    db.prepare("delete_one", sql)
+    db.exec_prepared("delete_one", values)
+    db.close
+  end
+
+  def update
+    db = PG.connect( {dbname: 'property_agency', host: 'localhost'} )
+    sql = "UPDATE properties SET ( address,
+                                  value,
+                                  year_built,
+                                  for_sale)
+                              = ($1, $2, $3, $4)
+                              WHERE id = $5"
+    values = [@address, @value, @year_built, @for_sale, @id]
+    db.prepare("update", sql)
+    db.exec_prepared("update", values)
+    db.close
+  end
+
+  def Property.delete_all
+    db = PG.connect( {dbname: 'property_agency', host: 'localhost'} )
+    sql = "DELETE FROM properties"
+    db.prepare("delete_all", sql)
+    db.exec_prepared("delete_all")
+    db.close
+  end
+
 end
